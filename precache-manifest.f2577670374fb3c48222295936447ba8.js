b9946a4f76e9f9329ec53927725812ba4f2fd74b
GardLundh <gard.lundh@gmail.com>
2020-07-19 23:44:46 +0200

    Updates

---
+++ precache-manifest.f2577670374fb3c48222295936447ba8.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "db0ed0a2d5a360e7bed307aadb8f2ab2",
+    "url": "/ShakespeareGenerator/index.html"
+  },
+  {
+    "revision": "a282f81925cc43dda871",
+    "url": "/ShakespeareGenerator/static/css/main.5a385dbd.chunk.css"
+  },
+  {
+    "revision": "dff43ee9b98689c2fe53",
+    "url": "/ShakespeareGenerator/static/js/2.3149bb4e.chunk.js"
+  },
+  {
+    "revision": "3453b8997016469371284a28c0e873e2",
+    "url": "/ShakespeareGenerator/static/js/2.3149bb4e.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "a282f81925cc43dda871",
+    "url": "/ShakespeareGenerator/static/js/main.c3f889c8.chunk.js"
+  },
+  {
+    "revision": "093f96925f4f98c9d010",
+    "url": "/ShakespeareGenerator/static/js/runtime-main.aa3417c2.js"
+  },
+  {
+    "revision": "3a7df2f83ef2632bab51ade7bf16f926",
+    "url": "/ShakespeareGenerator/static/media/shakespeare.3a7df2f8.jpg"
+  }
+]);
